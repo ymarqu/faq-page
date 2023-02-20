0c6e98c2f25645441aef97e11cdb3cecae375d72
ymarquina <yeseniamrqna@gmail.com>
2023-02-19 19:03:40 -0800

    Add styles

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const btns = document.querySelectorAll(".btn");
 // const answer = document.querySelector(".answer-text");
-
+const question = document.querySelector(".q1");
 btns.forEach( btn => {
     btn.addEventListener("click", () => {
         let answer = btn.parentNode.parentNode.nextElementSibling.childNodes[0].nextElementSibling;
         answer.classList.toggle("expand");
+
  })
 });
